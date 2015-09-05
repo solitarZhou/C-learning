41fe2127c222f131b757af8a35982e75fbcce8d0
solitarZhou <einsamersolitarzhou@gmail.com>
2015-09-06 00:38:40 +0800

    C++learning firstday,learn the processData of C++

---
+++ processData/limits.cpp
@@ -0,0 +1,30 @@
+//limits.cpp -- some integer limits
+#include <iostream>
+#define ZERO 0
+#include <climits>          //use limits.h for older systems
+
+int main () {
+        using namespace std ;
+        short sam = SHRT_MAX ;
+        unsigned short sue = sam ;
+
+        cout << "Sam has " << sam << "dollars and Sue has " << sue ;
+        cout << "dollars deposited." << endl
+                    << "Add $1 to each account. " << endl << "Now" ;
+        sam = sam + 1 ;
+        sue = sue + 1 ;
+        cout << "Sam has " << sam << "dollars and Sue has" << sue ;
+        cout << "dollars deposited.\n Poor Sam!" << endl ;
+        sam = ZERO ;
+        sue = ZERO ;
+        cout << "Sam has " << sam << "dollars and Sue has " << sue ;
+        cout << "dollars deposited." << endl
+                    << "Take $1 to each account. " << endl << "Now" ;
+        sam = sam - 1 ;
+        sue = sue - 1 ;
+        cout << "Sam has " << sam << "dollars and Sue has" << sue ;
+        cout << "dollars deposited." << endl << "Lucky Sue!" << endl ;
+        return 0 ;
+
+}
+
